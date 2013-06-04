4a3c3bb7801d348e3641e742587605a9f6434e84
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2013-06-04 17:09:45 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,112 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//= require jquery.easing.1.3
+//= require jquery.isotope.min
+//= require jquery.stellar.min
+//= require waypoints
+//= require sorting
+
+
+
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+	
+	
+    jQuery('#mycarousel').jcarousel({
+        auto: 1000,
+        scroll: 1,
+        wrap: 'circular'
+    });
+	
+	
+    $("a[rel^='prettyPhoto']").prettyPhoto();
+
+	
+    $(".hover_img").live('mouseover',function(){
+        var info=$(this).find("img");
+        info.stop().animate({
+            opacity:0.21
+        },300);
+        $(".preloader").css({
+            'background':'none'
+        });
+    }
+    );
+    $(".hover_img").live('mouseout',function(){
+        var info=$(this).find("img");
+        info.stop().animate({
+            opacity:1
+        },300);
+        $(".preloader").css({
+            'background':'none'
+        });
+    }
+    );
+		
+	
+});
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EaTechnologies::Application.config.secret_token = 'b9b3a538987a9a7eb886fef9b7cb32ad4397aae9a99ee969383d46ad474a91b4b627688f6409c3edc51af5707b9a4aa96d7a11932d6f1c26616e99e6ade4c491'
